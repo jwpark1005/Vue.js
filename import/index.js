d961e3964d1b0db2418439c148d29acafe33fc62
jwpark1005 <pjwoo1004@naver.com>
2019-01-13 21:03:51 +0900

    ES6 Import/Export

---
+++ import/index.js
@@ -0,0 +1,9 @@
+import func from './js/function.js';
+import {xeno} from './js/text.js';
+
+debugger;
+
+export default {
+    func,
+    xeno
+}
---
+++ import/js/function.js
@@ -0,0 +1,24 @@
+export default {
+
+    plus : function (a, b) {
+        return a + b;
+    },
+
+    minus : function (a, b) {
+        return a - b;
+    },
+
+    show : function (text) {
+        console.log(text);
+        return;
+    },
+
+/*    show minus(a, b) {
+        return a - b;
+    }
+
+    function show(text) {
+        console.log(text);
+        return;
+    }*/
+}
